f844facf39a1a2f5344f451e34472f7f7c41a0b9
kkapoor84 <kkapoor84@gmail.com>
2019-03-16 20:25:15 +0530

    commented the remaining refactored code

--- UnitTestNDBProject/UnitTestNDBProject/Tests/HomePageTest.cs
+++ UnitTestNDBProject/UnitTestNDBProject/Tests/HomePageTest.cs
@@ -12,32 +12,32 @@ namespace UnitTestNDBProject.Tests
     [TestFixture]
     class HomePageTest : BaseTestClass
     {
-        public HomePageTest() : base(BrowserType.Chrome)
-        {
+        //public HomePageTest() : base(BrowserType.Chrome)
+        //{
 
-        }
+        //}
 
-        [OneTimeSetUp]
-        public void BeforeClass()
-        {
-            LP.Login("LoginScreen$", "AccountUserValidCredentails");
+        //[OneTimeSetUp]
+        //public void BeforeClass()
+        //{
+        //    LP.Login("LoginScreen$", "AccountUserValidCredentails");
 
-        }
+        //}
 
-        [Description("Validate all the Home Page tabs are clickable"),Category("Smoke"),Category("Regression")]
-        public void A3_VerifyHomePageTabs()
-        {
-            HP.VerifyHomePageTitle();
-            HP.ClickDashBoardTab();
-            Assert.True(HP.VerifyDashBoardTabIsClicked());
-            HP.ClickDepositSummaryTab();
-            Assert.True(HP.VerifyDepositSummaryTabIsClicked());
-            HP.ClickResourcesTab();
-            Assert.True(HP.VerifyResourceTabIsClicked());
-            HP.ClickSettingTab();
-            Assert.True(HP.VerifySettingTabIsClicked());
+        //[Description("Validate all the Home Page tabs are clickable"),Category("Smoke"),Category("Regression")]
+        //public void A3_VerifyHomePageTabs()
+        //{
+        //    HP.VerifyHomePageTitle();
+        //    HP.ClickDashBoardTab();
+        //    Assert.True(HP.VerifyDashBoardTabIsClicked());
+        //    HP.ClickDepositSummaryTab();
+        //    Assert.True(HP.VerifyDepositSummaryTabIsClicked());
+        //    HP.ClickResourcesTab();
+        //    Assert.True(HP.VerifyResourceTabIsClicked());
+        //    HP.ClickSettingTab();
+        //    Assert.True(HP.VerifySettingTabIsClicked());
 
-        }
+        //}
 
 
 
