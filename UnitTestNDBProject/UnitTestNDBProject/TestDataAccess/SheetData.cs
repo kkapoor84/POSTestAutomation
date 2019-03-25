daacc577b3be7fdd8b0456f1818275ab73c20d83
kkapoor84 <kkapoor84@gmail.com>
2019-03-25 20:36:55 +0530

    updated the solution name from UnitTestNDBProject.sln to NDBPOSAutomation.csproj and same updated for projectname aswell

---
+++ UnitTestNDBProject/UnitTestNDBProject/TestDataAccess/SheetData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestNDBProject.TestDataAccess
+{
+    public class SheetData
+    {
+        public string Key { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
+
